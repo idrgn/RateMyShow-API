5f9cdfba4a30c49554180885052ffcec13f5ded0
idrgn <37042855+idrgn@users.noreply.github.com>
2023-01-20 13:15:29 +0100

    Añadida url /titles/{titleId}
    Añadida como URL de ejemplo

--- django/RateMyShow/RateMyShow/urls.py
+++ django/RateMyShow/RateMyShow/urls.py
@@ -1,3 +1,5 @@
+from webserviceapp import views
+
 """RateMyShow URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,4 +20,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('titles/<str:title_id>', views.get_title_by_id),
+    ]
--- django/RateMyShow/webserviceapp/views.py
+++ django/RateMyShow/webserviceapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.forms.models import model_to_dict
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Titles
+
+def get_title_by_id(r, title_id):
+    # Obtiene el título
+    title = Titles.objects.get(id=title_id)
+
+    # Se transforma el modelo a un diccionario
+    response = model_to_dict(title)
+
+    # Se envía la respuesta
+    return JsonResponse(
+        response,
+        json_dumps_params={"ensure_ascii": False},
+    )
