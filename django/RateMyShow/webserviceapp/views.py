5a5281280e36c8eb2b4bbfc407be8199581cf749
idrgn <37042855+idrgn@users.noreply.github.com>
2023-01-21 13:46:23 +0100

    Almacenamiento de cover y descripción en la BBDD

--- django/RateMyShow/webserviceapp/views.py
+++ django/RateMyShow/webserviceapp/views.py
@@ -1,9 +1,8 @@
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
+from bs4 import BeautifulSoup
 
 from .models import Titles
-
-from bs4 import BeautifulSoup
 
 import requests
 
@@ -12,47 +11,48 @@ def get_title_by_id(r, title_id):
     # Obtiene el título
     title = Titles.objects.get(id=title_id)
 
+    # Si los datos adicionales no existen en la BBDD obtienen de la web
+    if title.cover == None or title.description == None:
+
+        # Se definen headers
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+        }
+
+        # Se intenta hacer la petición
+        try:
+            request_response = requests.get(
+                f"http://www.imdb.com/title/{title_id}/", headers=headers
+            )
+            soup = BeautifulSoup(request_response.text, "html.parser")
+
+            # Se intenta obtener el cover
+            try:
+                img_tag = soup.find("img", class_="ipc-image")
+                img_src = img_tag["src"]
+                title.cover = img_src
+                title.save()
+            except Exception:
+                pass
+
+            # Se intenta obtener la descripción
+            try:
+                meta_description = soup.find("meta", attrs={"name": "description"})
+                content = meta_description["content"]
+                title.description = content
+                title.save()
+            except Exception:
+                pass
+
+        except Exception:
+            pass
+
     # Se transforma el modelo a un diccionario
     response = model_to_dict(title)
-
-    # Se añaden los valores por defecto
-    response["cover"] = "none"
-    response["description"] = "none"
-
-    # Se definen headers
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1",
-    }
-
-    # Se obtienen los datos de la web
-    try:
-        request_response = requests.get(
-            f"http://www.imdb.com/title/{title_id}/", headers=headers
-        )
-        soup = BeautifulSoup(request_response.text, "html.parser")
-
-        # Se intenta obtener el cover
-        try:
-            img_tag = soup.find("img", class_="ipc-image")
-            img_src = img_tag["src"]
-            response["cover"] = img_src
-        except Exception:
-            pass
-
-        # Se intenta obtener la description
-        try:
-            meta_description = soup.find("meta", attrs={"name": "description"})
-            content = meta_description["content"]
-            response["description"] = content
-        except Exception:
-            pass
-
-    except Exception:
-        pass
 
     # Se envía la respuesta
     return JsonResponse(
