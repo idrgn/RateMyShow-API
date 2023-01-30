c597d63980d5c97097f0116459b580e0f5ba705f
enriquerivasf <92723398+enriquerivasf@users.noreply.github.com>
2023-01-30 20:45:39 +0100

    Añadidas páginas en followers

--- django/RateMyShow/webserviceapp/views.py
+++ django/RateMyShow/webserviceapp/views.py
@@ -383,8 +383,24 @@ def get_followers_by_name(r, username):
             # Si genera un error al obtener el usuario, devuelve notfound
             return JsonResponse({"message": "Not found"}, status=404)
 
+        # Se obtiene la página actual
+        page = r.GET.get("page", 0)
+
+        # Si es string, intenta convertirla a número
+        if isinstance(page, str):
+            try:
+                page = int(page)
+            except Exception:
+                page = 0
+
+        # Cantidad de resultados por página
+        amount_per_page = 15
+
         # Obtiene todos los seguidores del usuario
         followers = Followers.objects.filter(followedid=user)
+
+        # Se obtiene el total de seguidores
+        total = followers.count()
 
         follower_list = []
         for follower in followers:
@@ -398,12 +414,16 @@ def get_followers_by_name(r, username):
             # Se añade dictionary
             follower_list.append(dictionary)
 
-        # Devuelve la lista de usuarios
+        # Devuelve la lista de seguidores
         return JsonResponse(
-            follower_list,
+            {
+                "total": total,
+                "pages": int(math.ceil(total / amount_per_page)),
+                "current": page,
+                "followers": follower_list,
+            },
             json_dumps_params={"ensure_ascii": False},
             status=200,
-            safe=False,
         )
 
 
