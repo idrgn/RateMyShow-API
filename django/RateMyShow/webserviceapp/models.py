663b2fe6feaa248d7013d96e3e129d0f4dc286d1
idrgn <37042855+idrgn@users.noreply.github.com>
2023-02-03 22:45:28 +0100

    Actualizados modelos de la BBDD

--- django/RateMyShow/webserviceapp/models.py
+++ django/RateMyShow/webserviceapp/models.py
@@ -40,6 +40,9 @@ class Followers(models.Model):
     followedid = models.ForeignKey(
         "Users", models.DO_NOTHING, db_column="followedId", related_name="followed"
     )  # Field name made lowercase.
+    addeddate = models.DateTimeField(
+        db_column="addedDate"
+    )  # Field name made lowercase.
 
     class Meta:
         managed = False
@@ -159,6 +162,14 @@ class Titles(models.Model):
         db_column="runtimeMinutes"
     )  # Field name made lowercase.
     language = models.CharField(max_length=255)
+    region = models.CharField(max_length=255)
+    isoriginaltitle = models.IntegerField(
+        db_column="isOriginalTitle"
+    )  # Field name made lowercase.
+    imdbrating = models.FloatField(db_column="imdbRating")  # Field name made lowercase.
+    imdbratingcount = models.IntegerField(
+        db_column="imdbRatingCount"
+    )  # Field name made lowercase.
     cover = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
 
@@ -213,119 +224,3 @@ class Users(models.Model):
 
         # Se comprueba que las contraseñas coinciden
         return bcrypt.checkpw(raw_password.encode("utf-8"), selfpw)
-
-
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = "auth_group"
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey("AuthPermission", models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = "auth_group_permissions"
-        unique_together = (("group", "permission"),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey("DjangoContentType", models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = "auth_permission"
-        unique_together = (("content_type", "codename"),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = "auth_user"
-
-
-class AuthUserGroups(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = "auth_user_groups"
-        unique_together = (("user", "group"),)
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = "auth_user_user_permissions"
-        unique_together = (("user", "permission"),)
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey(
-        "DjangoContentType", models.DO_NOTHING, blank=True, null=True
-    )
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = "django_admin_log"
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = "django_content_type"
-        unique_together = (("app_label", "model"),)
-
-
-class DjangoMigrations(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = "django_migrations"
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = "django_session"
