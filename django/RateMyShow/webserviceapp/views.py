42217c1c4067fbf4986f3321bdb2e38da95de8a0
idrgn <37042855+idrgn@users.noreply.github.com>
2023-02-07 13:07:57 +0100

    Clave primaria de avatarid

--- django/RateMyShow/webserviceapp/views.py
+++ django/RateMyShow/webserviceapp/views.py
@@ -1091,7 +1091,7 @@ def get_ratings(r, title_id):
                     "addedDate": rating.addeddate,
                     "poster": {
                         "username": rating.posterid.username,
-                        "avatarId": rating.posterid.avatarid,
+                        "avatarId": rating.posterid.avatarid.pk,
                     },
                 }
             )
